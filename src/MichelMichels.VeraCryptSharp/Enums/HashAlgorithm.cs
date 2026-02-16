8dcabc53bfb791f8e830bdf2c884df560e342031
Michel Michels <michel@miche.ls>
2026-02-16 19:59:34 +0100

    Project structur fix + .NET 10 upgrade

---
+++ src/MichelMichels.VeraCryptSharp/Enums/HashAlgorithm.cs
@@ -0,0 +1,21 @@
+﻿using MichelMichels.CliSharp.Attributes;
+
+namespace MichelMichels.VeraCryptSharp.Enums;
+
+public enum HashAlgorithm
+{
+    [ParameterName("")]
+    Auto,
+
+    [ParameterName("sha256")]
+    SHA256,
+
+    [ParameterName("sha512")]
+    SHA512,
+
+    [ParameterName("whirlpool")]
+    Whirlpool,
+
+    [ParameterName("ripemd160")]
+    RIPEMD160,
+}
---
+++ src/MichelMichels.VeraCryptSharp/IVeraCrypt.cs
@@ -0,0 +1,14 @@
+﻿using MichelMichels.CliSharp.Core;
+using MichelMichels.VeraCryptSharp.Enums;
+using System.Collections.Generic;
+
+namespace MichelMichels.VeraCryptSharp;
+
+public interface IVeraCrypt
+{
+    void Mount(string filePath, string password, HashAlgorithm hashAlgorithm = HashAlgorithm.Auto, string driveLetter = "V", bool isSilent = true);
+    void MountSecure(string filePath, HashAlgorithm hashAlgorithm = HashAlgorithm.Auto, string driveLetter = "V", bool useSecureDesktop = false);
+    void Dismount(string driveLetter, bool isSilent = true);
+    void DismountAll();
+    void Execute(IEnumerable<CliCommandLineSwitch> switches);
+}
